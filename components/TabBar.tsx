e640caa20b19e1ae9e3ee8922ddca2f0bf9b27e4
Ava <ava@seriez.app>
2026-06-13 21:45:21 -0700

    Feed icon: same active filter behavior as all other TabBar icons

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -46,7 +46,7 @@ export default function TabBar() {
                 active ? "text-[#a855f7]" : "text-[#9ca3af] hover:text-white"
               }`}
             >
-              <span className={`text-xl ${active && tab.name !== "Feed" ? "[&>img]:filter [&>img]:brightness-0 [&>img]:saturate-100 [&>img]:invert-[33%] [&>img]:sepia-[54%] [&>img]:saturate-[2050%] [&>img]:hue-rotate-[245deg] [&>img]:brightness-[.95] [&>img]:contrast-[.93]" : ""}`}>
+              <span className={`text-xl ${active ? "[&>img]:filter [&>img]:brightness-0 [&>img]:saturate-100 [&>img]:invert-[33%] [&>img]:sepia-[54%] [&>img]:saturate-[2050%] [&>img]:hue-rotate-[245deg] [&>img]:brightness-[.95] [&>img]:contrast-[.93]" : ""}`}>
                 {tab.icon}
               </span>
               <span className="font-medium">{tab.name}</span>
